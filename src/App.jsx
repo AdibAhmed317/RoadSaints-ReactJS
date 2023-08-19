5f6e0c246bc6f1104f70fa541f8738b8800a4bae
AdibAhmed317 <ahmedadib317@gmail.com>
2023-08-19 17:23:28 +0600

    folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className='bg-red-200'>
       <h1 className='text-6xl'>RoadSaints</h1>
+      <p className='text-4xl'>Welcome to RoadSaints</p>
     </div>
   );
 }
