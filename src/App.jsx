27c0e4b7fd8cfd41395795a484b3f1f83a560db8
AdibAhmed317 <ahmedadib317@gmail.com>
2023-08-19 17:19:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className='bg-red-200'>
+      <h1 className='text-6xl'>RoadSaints</h1>
+    </div>
+  );
+}
+
+export default App;
